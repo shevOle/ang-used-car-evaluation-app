54fc9f0cfe15ff00b5847672881e1d3b5596a5ba
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-15 22:45:15 +0200

    feat: add report item component

---
+++ src/app/report/report.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Report } from './interfaces/report';
+
+@Component({
+  selector: 'ucea-report',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './report.component.html',
+  styleUrl: './report.component.scss',
+})
+export class ReportComponent {
+  @Input() report!: Report;
+
+  map = 'map';
+}
