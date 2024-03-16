f9230874e7e0299091ad74f18347493c0e937447
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-16 16:44:53 +0200

    feat: add map to report component

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,14 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 import { Report } from './interfaces/report';
+import { Location } from './interfaces/location';
 
 @Component({
   selector: 'ucea-report',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GoogleMap, MapMarker],
+  inputs: [{ name: 'report', required: true }],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss',
 })
 export class ReportComponent {
-  @Input() report!: Report;
+  @Input()({ required: true })
+  public report!: Report;
+
+  zoom: number = 10;
+  center: Location = { lat: this.report?.lat, lng: this.report?.lng };
+  markerPosition: Location = {
+    lat: this.report?.lat,
+    lng: this.report?.lng,
+  };
 }
