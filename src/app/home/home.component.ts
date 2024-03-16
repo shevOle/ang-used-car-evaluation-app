1db97cd4bfa052d008a37aa743848c307943a1d5
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-16 19:10:40 +0200

    refactor: remove extensive home/ files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ReportsListComponent } from '../reportsList/reportsList.component';
 
 @Component({
   selector: 'ucea-home',
   standalone: true,
-  imports: [ReportsListComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  imports: [RouterModule, ReportsListComponent],
+  template: ` <section>Welcome to Home page!</section>`,
+  styles: `
+    section {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 200px;
+      font-size: 32px;
+      font-weight: 700;
+    }
+  `,
 })
 export class HomeComponent {}
