d87fb9ce47520c829b29e92c55c50718d7b1dfd6
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-17 18:40:24 +0200

    feat: add elements to report page

--- src/app/reportPage/reportPage.component.ts
+++ src/app/reportPage/reportPage.component.ts
@@ -2,7 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Report } from '../report/interfaces/report';
-import { ReportsListService } from '../reportsList/reportsList.service';
+import { ReportService } from '../services/reportsList.service';
+
+const moneyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 @Component({
   selector: 'ucea-reportPage',
@@ -12,12 +17,16 @@ import { ReportsListService } from '../reportsList/reportsList.service';
   styleUrl: './reportPage.component.scss',
 })
 export class ReportPageComponent {
-  reportsService: ReportsListService = inject(ReportsListService);
+  reportsService: ReportService = inject(ReportService);
   route: ActivatedRoute = inject(ActivatedRoute);
   report: Report | undefined;
 
   constructor() {
     const reportId: string = this.route.snapshot.params['id'];
     this.report = this.reportsService.getReportById(reportId);
+
+    if (this.report?.price) {
+      this.report.price = moneyFormat.format(this.report.price) as any;
+    }
   }
 }
