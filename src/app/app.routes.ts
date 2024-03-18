fab1b363182da807b4c3db02f3fc6e5d048dc9f1
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-18 23:39:04 +0200

    feat: add route for estimate page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReportsListComponent } from './reportsList/reportsList.component';
 import { ReportPageComponent } from './reportPage/reportPage.component';
+import { ReportEstimateComponent } from './report-estimate/report-estimate.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     path: 'reports/:id',
     component: ReportPageComponent,
     title: 'Report details',
+  },
+  {
+    path: 'estimate',
+    component: ReportEstimateComponent,
+    title: 'Get estimate price',
   },
 ];
