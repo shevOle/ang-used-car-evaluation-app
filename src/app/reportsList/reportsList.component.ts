53cb1dfb96e9bdbe06b41d465f4035e6be884e97
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-24 13:55:27 +0200

    feat: add form for reports filtering

--- src/app/reportsList/reportsList.component.ts
+++ src/app/reportsList/reportsList.component.ts
@@ -1,22 +1,50 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ReportService } from '../services/reports.service';
 import { Report } from '../interfaces/report';
 import { ReportComponent } from '../report/report.component';
+import { minProductionYear } from '../helpers/constants';
 
 @Component({
   selector: 'ucea-reportsList',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReportComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, ReportComponent],
   templateUrl: './reportsList.component.html',
   styleUrl: './reportsList.component.scss',
 })
 export class ReportsListComponent {
   protected reportsList: Report[] = [];
   protected reportsListService: ReportService = inject(ReportService);
+  filterForm = new FormGroup({
+    make: new FormControl('', { nonNullable: true }),
+    model: new FormControl('', { nonNullable: true }),
+    year: new FormControl<number | null>(null),
+  });
 
   constructor() {
     this.reportsList = this.reportsListService.getReports();
+  }
+
+  get availableYears() {
+    const yearLabels = [''];
+    for (let i = minProductionYear; i <= new Date().getFullYear(); i += 1) {
+      yearLabels.push(i.toString());
+    }
+    return yearLabels;
+  }
+
+  applyFilter() {
+    this.reportsList = this.reportsListService.getReports({
+      make: this.filterForm.value.make,
+      model: this.filterForm.value.model,
+      year: this.filterForm.value.year?.toString(),
+    });
+  }
+
+  resetFilter() {
+    this.filterForm.reset();
+    this.applyFilter();
   }
 }
