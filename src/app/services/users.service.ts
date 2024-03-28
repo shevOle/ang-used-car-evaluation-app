2516a651fe1c514907e5a6e6f833fd0c9a903ea0
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-03-28 19:51:34 +0200

    feat: add method placeholder for sign up

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IUserLoginInput } from '../interfaces/login-input';
+import { IUserSignUpInput } from '../interfaces/signup-input';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,9 @@ import { IUserLoginInput } from '../interfaces/login-input';
 export class UsersService {
   loginUser(input: IUserLoginInput) {
     console.log('logged in');
+  }
+
+  signUp(input: IUserSignUpInput) {
+    console.log('signed up');
   }
 }
