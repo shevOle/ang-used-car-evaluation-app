fada246e90d31b5f660b3c57172f830af4d093e9
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-03 20:48:25 +0300

    feat: connect auth service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthService } from './services/auth.service';
+import { IUser } from './interfaces/user';
 
 @Component({
   selector: 'ucea-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent],
+  imports: [CommonModule, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'UCEA';
-  router: Router = inject(Router);
+  currentUser: Partial<IUser> | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.currentUser.subscribe((x) => (this.currentUser = x));
+  }
 
   logOut() {
-    this.router.navigate(['login']);
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
   toggleMenu() {
