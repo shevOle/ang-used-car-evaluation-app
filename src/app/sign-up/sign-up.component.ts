b0b45ccf0c995ab9568e6c0b6348e6bf3f4634fe
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-08 19:05:00 +0300

    feat: implement basic slider logic and picture choosing process

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -30,6 +30,19 @@ export class SignUpComponent {
       validators: [Validators.required, Validators.minLength(3)],
     }),
   });
+  profilePictures = [
+    { url: '../assets/profile/yoda.png', name: 'yoda' },
+    { url: '../assets/profile/luke.png', name: 'luke' },
+    { url: '../assets/profile/chewie.png', name: 'chewie' },
+    { url: '../assets/profile/leya.png', name: 'leya' },
+    { url: '../assets/profile/solo.png', name: 'solo' },
+    { url: '../assets/profile/mando.png', name: 'mando' },
+  ];
+  chosenPictureIndex = 0;
+
+  get picture() {
+    return this.profilePictures[this.chosenPictureIndex];
+  }
 
   get email() {
     return this.signUpForm.get('email')!;
@@ -37,6 +50,22 @@ export class SignUpComponent {
 
   get password() {
     return this.signUpForm.get('password')!;
+  }
+
+  scrollImagesBackwards() {
+    if (this.chosenPictureIndex === 0) {
+      return (this.chosenPictureIndex = this.profilePictures.length - 1);
+    }
+
+    return --this.chosenPictureIndex;
+  }
+
+  scrollImagesForward() {
+    if (this.chosenPictureIndex === this.profilePictures.length - 1) {
+      return (this.chosenPictureIndex = 0);
+    }
+
+    return ++this.chosenPictureIndex;
   }
 
   signUp() {
