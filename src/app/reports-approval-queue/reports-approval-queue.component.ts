0e3c106931fa679a4b7f4135a237efd799b631a8
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-10 21:43:13 +0300

    feat: add report approval queue component templates

---
+++ src/app/reports-approval-queue/reports-approval-queue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ucea-reports-approval-queue',
+  standalone: true,
+  imports: [],
+  templateUrl: './reports-approval-queue.component.html',
+  styleUrl: './reports-approval-queue.component.scss'
+})
+export class ReportsApprovalQueueComponent {
+
+}
