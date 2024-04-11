80fe99a2ba6c8d0edd7a1e49876840c847965c38
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-11 20:41:58 +0300

    feat: add approval page component logic for fetching  reports data

--- src/app/reports-approval-queue/reports-approval-queue.component.ts
+++ src/app/reports-approval-queue/reports-approval-queue.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportService } from '../services/reports.service';
+import { Report } from '../interfaces/report';
 
 @Component({
   selector: 'ucea-reports-approval-queue',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reports-approval-queue.component.html',
-  styleUrl: './reports-approval-queue.component.scss'
+  styleUrl: './reports-approval-queue.component.scss',
 })
 export class ReportsApprovalQueueComponent {
+  reports: Report[] = [];
+  constructor(private reportsService: ReportService) {}
 
+  async ngOnInit() {
+    this.reports = await this.reportsService.getNewReports();
+  }
 }
