f8f7c7fa9a7bc6495089c9f71b97fa43f97bd078
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-13 00:12:21 +0300

    feat: add logic for showing action menu

--- src/app/reports-approval-queue/reports-approval-queue.component.ts
+++ src/app/reports-approval-queue/reports-approval-queue.component.ts
@@ -12,9 +12,23 @@ import { Report } from '../interfaces/report';
 })
 export class ReportsApprovalQueueComponent {
   reports: Report[] = [];
+  actionMenuElements: NodeListOf<HTMLElement> | null = null;
   constructor(private reportsService: ReportService) {}
 
   async ngOnInit() {
     this.reports = await this.reportsService.getNewReports();
+    document
+      .querySelector('body')
+      ?.addEventListener('click', () => this.hideActionMenu());
+  }
+
+  showActionMenu(id: string) {
+    const el = document.getElementById(id);
+    el?.classList.toggle('visible');
+  }
+
+  hideActionMenu() {
+    // const elements = document.querySelectorAll('.report-table-action-menu');
+    // elements?.forEach((i) => i.classList.remove('visible'));
   }
 }
