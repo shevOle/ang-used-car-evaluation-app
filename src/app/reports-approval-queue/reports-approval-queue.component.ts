974c6241be0733bc32907cbd692c171208f5e864
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-17 19:20:06 +0300

    feat: add default paginator

--- src/app/reports-approval-queue/reports-approval-queue.component.ts
+++ src/app/reports-approval-queue/reports-approval-queue.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {
   CommonModule,
   CurrencyPipe,
   TitleCasePipe,
   DecimalPipe,
 } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { ReportService } from '../services/reports.service';
 import { Report } from '../interfaces/report';
@@ -24,12 +25,16 @@ import { Report } from '../interfaces/report';
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
+    MatPaginatorModule,
   ],
   templateUrl: './reports-approval-queue.component.html',
   styleUrl: './reports-approval-queue.component.scss',
 })
 export class ReportsApprovalQueueComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   reportsData: Report[] = [];
+  dataSource = new MatTableDataSource<Report>(this.reportsData);
   columns: string[] = [
     'maker-col',
     'model-col',
@@ -40,6 +45,10 @@ export class ReportsApprovalQueueComponent {
   ];
 
   constructor(private reportsService: ReportService) {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   async ngOnInit() {
     this.reportsData = await this.reportsService.getNewReports();
