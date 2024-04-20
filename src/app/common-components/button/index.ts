52946ea1a2175769e7609ef4b77a6b17ff63bcfc
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-20 22:15:31 +0300

    fix: correct common button styles

--- src/app/common-components/button/index.ts
+++ src/app/common-components/button/index.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ucea-button',
   templateUrl: 'index.html',
-  styles: '',
+  styleUrl: 'index.scss',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, TitleCasePipe],
 })
 export class CommonButton {
-  @Input({ required: true }) type!: 'primary' | 'secondary' | 'cancel';
+  @Input({ required: true }) visual!: 'main' | 'secondary' | 'cancel';
   @Input({ required: true }) label!: string;
   @Input({ required: true }) onClick!: Function;
   @Input() disabled: boolean = false;
