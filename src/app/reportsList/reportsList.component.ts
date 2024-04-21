275fd4d13a7bc5ff188cdc435fff2716105d1550
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-21 18:47:21 +0300

    refactor: use mui form fields for reports page filter

--- src/app/reportsList/reportsList.component.ts
+++ src/app/reportsList/reportsList.component.ts
@@ -1,26 +1,51 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  FormGroup,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReportService } from '../services/reports.service';
 import { Report } from '../interfaces/report';
 import { ReportComponent } from '../report/report.component';
 import { minProductionYear } from '../helpers/constants';
+import { CommonButton } from '../common-components/button';
 
 @Component({
   selector: 'ucea-reportsList',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReportComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReportComponent,
+    CommonButton,
+  ],
   templateUrl: './reportsList.component.html',
   styleUrl: './reportsList.component.scss',
 })
 export class ReportsListComponent {
   protected $reports!: Promise<Report[]>;
 
+  filterForm = new FormGroup({
+    make: new FormControl('', { nonNullable: true }),
+    model: new FormControl('', { nonNullable: true }),
+    year: new FormControl('', { nonNullable: true }),
+  });
+
   constructor(protected reportsService: ReportService) {}
 
   get availableYears() {
-    const yearLabels = [''];
+    const yearLabels = [];
     for (let i = minProductionYear; i <= new Date().getFullYear(); i += 1) {
       yearLabels.push(i.toString());
     }
@@ -31,14 +56,17 @@ export class ReportsListComponent {
     this.$reports = this.reportsService.getAllReports();
   }
 
-  applyFilter(make: string, model: string, year: string) {
-    this.$reports = this.reportsService.getFilteredReports(make, model, year);
+  applyFilter() {
+    const { make, model, year } = this.filterForm.value!;
+    this.$reports = this.reportsService.getFilteredReports(
+      make!,
+      model!,
+      year!
+    );
   }
 
-  resetFilter(fieldsList: (HTMLInputElement | HTMLSelectElement)[]) {
-    fieldsList.forEach((field) => {
-      field.value = '';
-    });
+  resetFilter() {
+    this.filterForm.reset();
     this.$reports = this.reportsService.getAllReports();
   }
 }
