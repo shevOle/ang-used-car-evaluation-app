1787edcbd5dcd157f06c54828e03b076688a6b51
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-22 23:58:47 +0300

    refactor: use mui components for navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './home/home.component';
 import { AuthService } from './services/auth.service';
 import { IUser } from './interfaces/user';
@@ -8,7 +11,14 @@ import { IUser } from './interfaces/user';
 @Component({
   selector: 'ucea-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, HomeComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HomeComponent,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -23,6 +33,7 @@ export class AppComponent {
       type: 'link',
       click: this.closeBurgerMenu.bind(this),
       title: 'Reports',
+      icon: 'article',
     },
     {
       link: '/estimate',
@@ -31,6 +42,7 @@ export class AppComponent {
       type: 'link',
       click: this.closeBurgerMenu.bind(this),
       title: 'Estimate',
+      icon: 'calculate',
     },
     {
       link: '/add-report',
@@ -39,6 +51,7 @@ export class AppComponent {
       type: 'link',
       click: this.closeBurgerMenu.bind(this),
       title: 'Add report',
+      icon: 'note_add',
     },
     {
       link: '/report-approval-queue',
@@ -47,6 +60,7 @@ export class AppComponent {
       type: 'link',
       click: this.closeBurgerMenu.bind(this),
       title: 'Reports Approvals',
+      icon: 'fact_check',
     },
     {
       link: '/profile',
@@ -55,22 +69,16 @@ export class AppComponent {
       type: 'link',
       click: this.closeBurgerMenu.bind(this),
       title: 'Profile',
+      icon: 'manage_accounts',
     },
     {
       link: '',
       isRendered: true,
-      class: 'button-default button-logout',
+      class: 'logout-button',
       type: 'button',
       click: this.logOut.bind(this),
-      title: 'Logout button',
-    },
-    {
-      link: '',
-      isRendered: true,
-      class: 'button-default button-close-menu',
-      type: 'button',
-      click: this.closeBurgerMenu.bind(this),
-      title: 'Close menu',
+      title: 'Logout',
+      icon: 'logout',
     },
   ];
 
