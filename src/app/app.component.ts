d9e82089150e562f1bfecc1c4c530604106c712f
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-04-22 23:06:10 +0300

    refactor: small reformatting of nav element construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,64 @@ import { IUser } from './interfaces/user';
 export class AppComponent {
   navElement: Element | null = null;
   currentUser: Partial<IUser> | null = null;
+  navComponents = [
+    {
+      link: '/reports',
+      isRendered: true,
+      class: 'site-header-link',
+      type: 'link',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Reports',
+    },
+    {
+      link: '/estimate',
+      isRendered: true,
+      class: 'site-header-link',
+      type: 'link',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Estimate',
+    },
+    {
+      link: '/add-report',
+      isRendered: true,
+      class: 'site-header-link',
+      type: 'link',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Add report',
+    },
+    {
+      link: '/report-approval-queue',
+      isRendered: this.currentUser?.isAdmin,
+      class: 'site-header-link',
+      type: 'link',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Reports Approvals',
+    },
+    {
+      link: '/profile',
+      isRendered: true,
+      class: 'site-header-link',
+      type: 'link',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Profile',
+    },
+    {
+      link: '',
+      isRendered: true,
+      class: 'button-default button-logout',
+      type: 'button',
+      click: this.logOut.bind(this),
+      title: 'Logout button',
+    },
+    {
+      link: '',
+      isRendered: true,
+      class: 'button-default button-close-menu',
+      type: 'button',
+      click: this.closeBurgerMenu.bind(this),
+      title: 'Close menu',
+    },
+  ];
 
   constructor(private authService: AuthService, private router: Router) {
     this.authService.currentUser.subscribe((x) => (this.currentUser = x));
