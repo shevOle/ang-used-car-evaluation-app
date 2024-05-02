27ed716c6843e9964d5eb51c63d0c7b815475a48
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-05-02 19:14:45 +0300

    refactor: replace embedded picture chooser for sign up page with common component

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -13,6 +13,7 @@ import { firstValueFrom } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { FormCard } from '../common-components/form-card';
 import { CommonButton } from '../common-components/button';
+import { PictureChooserComponent } from '../common-components/profile-picture-chooser';
 
 type FormControlFieldName = 'email' | 'password';
 
@@ -27,6 +28,7 @@ type FormControlFieldName = 'email' | 'password';
     MatInputModule,
     MatFormFieldModule,
     CommonButton,
+    PictureChooserComponent,
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
@@ -42,20 +44,11 @@ export class SignUpComponent {
       nonNullable: true,
       validators: [Validators.required, Validators.minLength(3)],
     }),
+    picture: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
   });
-  profilePictures = [
-    { url: '../assets/profile/yoda.png', name: 'yoda' },
-    { url: '../assets/profile/luke.png', name: 'luke' },
-    { url: '../assets/profile/chewie.png', name: 'chewie' },
-    { url: '../assets/profile/leya.png', name: 'leya' },
-    { url: '../assets/profile/solo.png', name: 'solo' },
-    { url: '../assets/profile/mando.png', name: 'mando' },
-  ];
-  chosenPictureIndex = 0;
-
-  get picture() {
-    return this.profilePictures[this.chosenPictureIndex];
-  }
 
   get email() {
     return this.signUpForm.get('email')!;
@@ -65,31 +58,20 @@ export class SignUpComponent {
     return this.signUpForm.get('password')!;
   }
 
-  scrollImagesBackwards() {
-    if (this.chosenPictureIndex === 0) {
-      return (this.chosenPictureIndex = this.profilePictures.length - 1);
-    }
-
-    return --this.chosenPictureIndex;
-  }
-
-  scrollImagesForward() {
-    if (this.chosenPictureIndex === this.profilePictures.length - 1) {
-      return (this.chosenPictureIndex = 0);
-    }
-
-    return ++this.chosenPictureIndex;
+  get picture() {
+    return this.signUpForm.get('picture')!;
   }
 
   async signUp() {
     const email = this.signUpForm.value.email!;
     const password = this.signUpForm.value.password!;
+    const picture = this.signUpForm.value.picture!;
 
     await firstValueFrom(
       this.authService.signUp({
         email,
         password,
-        profilePicture: this.profilePictures[this.chosenPictureIndex].url,
+        profilePicture: picture,
       })
     );
 
