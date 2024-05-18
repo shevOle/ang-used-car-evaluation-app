83812b2461a978bcba455390ed31bd83963fcbee
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-05-18 18:14:05 +0300

    fix: switch to backend app instead of mockapi

--- src/app/helpers/constants.ts
+++ src/app/helpers/constants.ts
@@ -1,4 +1,4 @@
 export const minProductionYear = 1990;
 export const maxSoldPrice = 1000000;
 export const maxMileage = 1000000;
-export const apiUrl = 'https://660801fea2a5dd477b13dc71.mockapi.io/api/v1';
+export const apiUrl = 'http://localhost:3000';
