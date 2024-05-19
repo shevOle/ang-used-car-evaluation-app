cb8c14408bb2c58557561f6c3b57632a85debf34
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-05-19 21:30:07 +0300

    refactor: integrate new API into auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,8 +21,25 @@ export class AuthService {
   public currentUser: Observable<IUser | null>;
 
   constructor(private httpClient: HttpClient, private router: Router) {
-    this.currentUserSubject = new BehaviorSubject({});
+    this.currentUserSubject = new BehaviorSubject(null);
     this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  saveUser() {
+    const token = getToken(document.cookie);
+    const userInfo = getPayload(token);
+
+    if (userInfo && userInfo.exp * 1000 < Date.now()) {
+      this.currentUserSubject.next(null);
+    }
+
+    this.currentUserSubject.next(userInfo);
+
+    const { queryParams } = this.router.parseUrl(
+      this.router.routerState.snapshot.url
+    );
+
+    this.router.navigate([queryParams['returnUrl'] || '/']);
   }
 
   public get currentUserValue(): IUser | null {
@@ -36,46 +53,47 @@ export class AuthService {
   }
 
   async loginUser(data: IUserLoginInput) {
-    try {
-      await firstValueFrom(
-        this.httpClient.post(`${this.url}/login`, data, {
+    await firstValueFrom(
+      this.httpClient.post(`${this.url}/login`, data, {
+        withCredentials: true,
+        observe: 'response',
+        responseType: 'text',
+      })
+    );
+
+    this.saveUser();
+  }
+
+  async logout() {
+    await firstValueFrom(
+      this.httpClient.post(
+        `${this.url}/logout`,
+        {},
+        {
           withCredentials: true,
-          observe: 'events',
-        })
-      );
-      this.saveUser();
+          observe: 'response',
+          responseType: 'text',
+        }
+      )
+    );
 
-      const { queryParams } = this.router.parseUrl(
-        this.router.routerState.snapshot.url
-      );
-
-      this.router.navigate([queryParams['returnUrl'] || '/']);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  saveUser() {
-    const token = getToken(document.cookie);
-    const userInfo = getPayload(token);
-
-    if (userInfo && userInfo.exp * 1000 < Date.now()) {
-      this.currentUserSubject.next(null);
-    }
-
-    this.currentUserSubject.next(userInfo);
-  }
-
-  logout() {
-    localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
   }
 
-  signUp(input: IUserSignUpInput) {
+  async signUp(input: IUserSignUpInput) {
     const userData = {
       ...input,
       isAdmin: false,
     };
-    return this.httpClient.post(`${this.url}/signup`, userData);
+
+    await firstValueFrom(
+      this.httpClient.post(`${this.url}/signup`, userData, {
+        withCredentials: true,
+        observe: 'response',
+        responseType: 'text',
+      })
+    );
+
+    this.saveUser();
   }
 }
