47afacd34f4f4209ebcdbf50f482c0980a8fbec8
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-06-08 15:41:36 +0300

    feat: connect google map to the report page

--- src/app/reportPage/reportPage.component.ts
+++ src/app/reportPage/reportPage.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 import { Report } from '../interfaces/report';
 import { ReportService } from '../services/reports.service';
 
 @Component({
   selector: 'ucea-reportPage',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, GoogleMap, MapMarker],
   templateUrl: './reportPage.component.html',
   styleUrl: './reportPage.component.scss',
 })
 export class ReportPageComponent {
-  protected $report!: Promise<Report>;
+  protected $report!: Report;
+  protected markerSpot!: google.maps.LatLngLiteral;
 
   constructor(
     protected reportsService: ReportService,
     protected route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const reportId: string = this.route.snapshot.params['id'];
-    this.$report = this.reportsService.getReportById(reportId);
+    this.$report = await this.reportsService.getReportById(reportId);
+    this.markerSpot = { lat: this.$report.lat, lng: this.$report.lng };
   }
 }
