b89ed6f1f6c5d4e60a317a906b8d628ea7b9bed2
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-06-23 14:19:19 +0300

    feat: replace background image with a map for report card

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -7,6 +7,7 @@ import {
   DecimalPipe,
 } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 import { Report } from '../interfaces/report';
 
 @Component({
@@ -19,10 +20,17 @@ import { Report } from '../interfaces/report';
     MatCardModule,
     TitleCasePipe,
     DecimalPipe,
+    GoogleMap,
+    MapMarker,
   ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss',
 })
 export class ReportComponent {
   @Input() report!: Report;
+  markerSpot!: google.maps.LatLngLiteral;
+
+  async ngOnInit(): Promise<void> {
+    this.markerSpot = { lat: this.report.lat, lng: this.report.lng };
+  }
 }
