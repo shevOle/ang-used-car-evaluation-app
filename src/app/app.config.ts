afeef0d32504ad7515f13b72062c2e69f3ce34db
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-06-26 19:46:52 +0300

    feat: connect toastr to the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideAnimationsAsync('noop')],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync('noop'),
+    provideAnimations(),
+    provideToastr(),
+  ],
 };
