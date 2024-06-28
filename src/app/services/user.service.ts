60c060b74ed99dd68d02ab7222b2ee582e19edcb
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-06-28 17:44:52 +0300

    feat: add notifications on user update action

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 import { IUpdateUserInput } from '../interfaces/updateUser-input';
 import { apiUrl } from '../helpers/constants';
 import { AuthService } from './auth.service';
@@ -13,21 +14,27 @@ export class UserService {
 
   constructor(
     private httpClient: HttpClient,
-    private authService: AuthService
+    private authService: AuthService,
+    private toastr: ToastrService
   ) {}
 
   async updateUser(input: IUpdateUserInput) {
-    const updateUrl = `${this.url}/${input.id}`;
-    const update = Object.assign({}, input) as any;
-    delete update.id;
+    try {
+      const updateUrl = `${this.url}/${input.id}`;
+      const update = Object.assign({}, input) as any;
+      delete update.id;
 
-    const observable = this.httpClient.patch(updateUrl, update, {
-      withCredentials: true,
-      observe: 'response',
-      responseType: 'text',
-    });
+      const observable = this.httpClient.patch(updateUrl, update, {
+        withCredentials: true,
+        observe: 'response',
+        responseType: 'text',
+      });
 
-    await firstValueFrom(observable);
-    this.authService.saveUser();
+      await firstValueFrom(observable);
+      this.authService.saveUser();
+      this.toastr.success('Data was successfully updated');
+    } catch (err: any) {
+      this.toastr.error(err);
+    }
   }
 }
