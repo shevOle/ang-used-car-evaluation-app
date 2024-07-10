1b4cf333c1de673f1e23eb0effd9f12e3d74dd8f
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-07-10 18:46:56 +0300

    feat: connect http interceptor to the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { httpErrorInterceptor } from './interceptors/http-error-response.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor])),
     provideAnimationsAsync('noop'),
     provideAnimations(),
     provideToastr(),
