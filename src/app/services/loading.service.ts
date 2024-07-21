8351addfb56ffc2e20428baf48fb42e8b4d8ca4a
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-07-21 14:28:17 +0300

    feat: add loading service

---
+++ src/app/services/loading.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+
+  loading$ = this.loadingSubject.asObservable();
+
+  get isLoading() {
+    return this.loadingSubject.value;
+  }
+
+  loadingOn() {
+    this.loadingSubject.next(true);
+  }
+
+  loadingOff() {
+    this.loadingSubject.next(false);
+  }
+}
