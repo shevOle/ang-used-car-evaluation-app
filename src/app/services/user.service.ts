7a6741b3e883d45e0a7b6e4eb18c8a833204b89e
Oleg Shevchenko <shevchenkoo.js@gmail.com>
2024-07-27 16:28:48 +0300

    feat: add loader to user service's methods

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import { ToastrService } from 'ngx-toastr';
 import { IUpdateUserInput } from '../interfaces/updateUser-input';
 import { apiUrl } from '../helpers/constants';
 import { AuthService } from './auth.service';
+import { LoadingService } from './loading.service';
 
 @Injectable({
   providedIn: 'root',
@@ -15,23 +16,29 @@ export class UserService {
   constructor(
     private httpClient: HttpClient,
     private authService: AuthService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private loadingService: LoadingService
   ) {}
 
   async updateUser(input: IUpdateUserInput) {
-    const updateUrl = `${this.url}/${input.id}`;
-    const update = Object.assign({}, input) as any;
-    delete update.id;
+    try {
+      this.loadingService.loadingOn();
+      const updateUrl = `${this.url}/${input.id}`;
+      const update = Object.assign({}, input) as any;
+      delete update.id;
 
-    const observable = this.httpClient.patch(updateUrl, update, {
-      withCredentials: true,
-      observe: 'response',
-      responseType: 'text',
-    });
+      const observable = this.httpClient.patch(updateUrl, update, {
+        withCredentials: true,
+        observe: 'response',
+        responseType: 'text',
+      });
 
-    await firstValueFrom(observable);
-    this.authService.saveUser();
+      await firstValueFrom(observable);
+      this.authService.saveUser();
 
-    this.toastr.success('User was successfully updated!');
+      this.toastr.success('User was successfully updated!');
+    } finally {
+      this.loadingService.loadingOff();
+    }
   }
 }
